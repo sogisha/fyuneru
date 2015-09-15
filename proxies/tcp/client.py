790eaa35ae7e685954cee63e8b44534dad52a84d
Sogisha <sogisha@india.com>
2015-09-15 04:18:18 +0200

    more coding

--- proxies/tcp/client.py
+++ proxies/tcp/client.py
@@ -3,9 +3,13 @@
 import argparse
 from select import select
 import socket
+import sys
+import time
 
 from dgram_stream import DatagramToStream, StreamToDatagram
 
+UDPCONNECTOR_WORD = \
+    "Across the Great Wall, we can reach every corner in the world."
 
 
 parser = argparse.ArgumentParser()
@@ -20,4 +24,35 @@ args = parser.parse_args()
 localSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 remoteSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+sendStream = DatagramToStream()
+recvDatagram = StreamToDatagram()
 
+while True:
+    print "Sending handshake words to local UDP port %d..." % args.localPort
+    localSocket.sendto(UDPCONNECTOR_WORD, ("127.0.0.1", args.localPort))
+    readable = select([localSocket], [], [])[0][0]
+    data, addr = readable.recvfrom(65536)
+    if data.strip() == UDPCONNECTOR_WORD:
+        print "Connected to %s:%d" % addr
+        break
+    time.sleep(0.5)
+
+
+##############################################################################
+
+def doLoop():
+    global localSocket, remoteSocket
+    reader = select([localSocket, remoteSocket], [], [])[0]
+    for each in reader:
+        if each == localSocket:
+            pass
+
+        if each == remoteSocket:
+            pass
+
+try:
+    while True:
+        doLoop()
+except Exception,e:
+    print e
+    sys.exit(0)
