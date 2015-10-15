70b011f8ca7b2167ad1eae7d73c8d9a15ee711cd
Sogisha <sogisha@india.com>
2015-10-16 00:48:04 +0200

    server ip may different from ss-tunnel server ip

--- fyuneru/proxyconf/drv_shadowsocks.py
+++ fyuneru/proxyconf/drv_shadowsocks.py
@@ -9,16 +9,16 @@ def proxyCommand(self, mode):
         self.baseKey,
         hashlib.sha256
     ).digest().encode('base64').strip()
-    serverBinary = self.proxyConfig["server"]["bin"]
-    clientBinary = self.proxyConfig["client"]["bin"]
+    serverBinary = self.proxyConfig["tunnel-server"]["bin"]
+    clientBinary = self.proxyConfig["tunnel-client"]["bin"]
 
     if mode == 's':
         proxyCommand = [
             serverBinary,
             '-k', sharedsecret,
             '-m', 'aes-256-cfb',
-            '-s', self.proxyConfig["server"]["ip"],
-            '-p', str(self.proxyConfig["server"]["port"]),
+            '-s', self.proxyConfig["tunnel-server"]["ip"],
+            '-p', str(self.proxyConfig["tunnel-client"]["port"]),
             '-U',
         ]
     else:
@@ -27,13 +27,13 @@ def proxyCommand(self, mode):
             os.path.join(self.proxyBase, 'shadowsocks', 'client.py'),
             '--bin', clientBinary,
             '-k', sharedsecret,
-            '-s', self.proxyConfig["server"]["ip"],
-            '-p', str(self.proxyConfig["server"]["port"]),
+            '-s', self.proxyConfig["tunnel-server"]["ip"],
+            '-p', str(self.proxyConfig["tunnel-server"]["port"]),
             '-b', '127.0.0.1',
-            '-l', str(self.proxyConfig["client"]["port"]),
+            '-l', str(self.proxyConfig["tunnel-client"]["port"]),
             '-m', 'aes-256-cfb',
-            str(self.portClient), # local  udp listening port
-            '127.0.0.1',          # remote udp listening addr
-            str(self.portServer), # remote udp listening port
+            str(self.portClient),       # local  udp listening port
+            self.proxyConfig["server"], # remote udp listening addr
+            str(self.portServer),       # remote udp listening port
         ]
     return proxyCommand
