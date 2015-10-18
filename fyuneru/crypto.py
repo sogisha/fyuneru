3fabf2efd40a01a7c761e031d938a15156d23c62
Sogisha <sogisha@india.com>
2015-10-18 21:23:17 +0200

    randomize packet size: not tested yet

--- fyuneru/crypto.py
+++ fyuneru/crypto.py
@@ -1,11 +1,46 @@
+"""
+Encryption
+
+    1. Cleartext:
+                                 | BUFFER arbitary length |
+                | DATALEN | HMAC |                        | RANDOM PADDING |
+     Bytes      |  2      |  32  | ?                      | ?              |
+
+    2. Encryption:
+                | ENCRYPTED BUFFER ....................................... |
+           | IV |
+     Bytes | 24 | ?                                                        |
+
+    3. Result:
+           | PSEUDORANDOM BUFFER OF RANDOM LENGTH ........................ |
+     Bytes | ?                                                             |
+
+"""
+
 import hashlib 
 import hmac
 from os import urandom
+from struct import pack, unpack
+import random
 
 from salsa20 import XSalsa20_xor
 
 _HASHALGO = hashlib.sha256
 _HASHLEN = len(_HASHALGO('').digest())
+
+_RESULT_SIZE = 0xFFFF - 24 - 2 - 32 
+
+
+class CryptoException(Exception): pass
+
+def decidePaddingLength(bufferLength):
+    global _RESULT_SIZE
+    minSize = 0
+    if bufferLength < 1600:
+        maxSize = 1500 
+    else:
+        maxSize = int(min(_RESULT_SIZE - bufferLength, bufferLength * 2.0))
+    return random.randint(minSize, maxSize)
 
 class Crypto:
     def __init__(self, passphrase):
@@ -20,9 +55,16 @@ class Crypto:
         return worker.digest()
 
     def encrypt(self, buf):
+        if len(buf) > _RESULT_SIZE: 
+            raise CryptoException('Buffer too large to be encrypted.')
+
         hmac = self.__HMAC(buf)
+        buflen = len(buf)
+        lenstr = pack('<H', buflen)
+        padding = '0' * decidePaddingLength(buflen) # TODO discuss security here!
+
         iv = urandom(24) # since IV will be exposed, has to be crypto. random.
-        cleartext = hmac + buf
+        cleartext = lenstr + hmac + buf + padding
         encrypted = XSalsa20_xor(cleartext, iv, self.__KEY)
         return iv + encrypted
 
@@ -32,8 +74,19 @@ class Crypto:
         iv = buf[:24]
         ciphertext = buf[24:]
         decrypted = XSalsa20_xor(ciphertext, iv, self.__KEY)
-        hmac = decrypted[:_HASHLEN]
-        buf = decrypted[_HASHLEN:]
+
+        lenstr = decrypted[:2]
+        buflen = unpack('<H', lenstr)[0]
+        hmac = decrypted[2:_HASHLEN+2]
+        buf = decrypted[_HASHLEN+2:][:buflen]
+        
         if self.__HMAC(buf) != hmac:
             return False
         return buf
+
+
+if __name__ == '__main__':
+    encryptor = Crypto('test')
+    decryptor = Crypto('test')
+    encrypted = encryptor.encrypt('hello, world' * 30)
+    print len(encrypted), decryptor.decrypt(encrypted)
