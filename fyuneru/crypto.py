63601c4bce080ae61bc9367425dd12aaaee673b4
Sogisha <sogisha@india.com>
2015-10-18 21:39:31 +0200

    adjust random range

--- fyuneru/crypto.py
+++ fyuneru/crypto.py
@@ -37,7 +37,7 @@ def decidePaddingLength(bufferLength):
     global _RESULT_SIZE
     minSize = 0
     if bufferLength < 1600:
-        maxSize = 1500 
+        maxSize = 800 
     else:
         maxSize = int(min(_RESULT_SIZE - bufferLength, bufferLength * 2.0))
     return random.randint(minSize, maxSize)
