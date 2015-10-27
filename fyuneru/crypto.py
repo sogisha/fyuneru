0f944a5083662c99ed82777005cf76843920e48c
Sogisha <sogisha@users.noreply.github.com>
2015-10-27 23:37:23 +0100

    decided to try an ICMP proxy, and changed encryption schema

--- fyuneru/crypto.py
+++ fyuneru/crypto.py
@@ -1,33 +1,55 @@
 """
-Encryption
+Encryption Schema
+=================
 
     1. Cleartext:
-                                 | BUFFER arbitary length |
-                | DATALEN | HMAC |                        | RANDOM PADDING |
-     Bytes      |  2      |  32  | ?                      | ?              |
+                          | BUFFER arbitary length |
+                | DATALEN |                        | RANDOM PADDING |
+     Bytes      |  2      | ?                      | ?              |
 
     2. Encryption:
-                | ENCRYPTED BUFFER ....................................... |
-           | IV |
-     Bytes | 24 | ?                                                        |
+                       | ENCRYPTED BUFFER ................................ |
+           | IV | HMAC |
+     Bytes | 24 |  20  |                                                   |
 
     3. Result:
            | PSEUDORANDOM BUFFER OF RANDOM LENGTH ........................ |
      Bytes | ?                                                             |
 
+Remarks
+=======
+
+1. We use HMAC-SHA1 to authenticate the encrypted buffer in following reasons:
+1.1 A good HMAC should be indistinguishable against random bytes without
+    knowledge of key. Putting it outside the cleartext and making it to
+    authenticate the cihpertext makes it easier(with a known HMAC key!) to find
+    out invalid packets without decryption.
+1.2 This is secure enough. SHA1 is attacked only in theory, and that's even not
+    HMAC(HMAC-MD5 is still secure enough, think of that). Even an attacker has
+    got some advances in theory, it doesn't mean he will be able to use it
+    detecting a real-time traffic effectively(making it hard to do deep packet
+    analysis).
+1.3 Unrevealing the HMAC key will not lead to the leak of encryption key, since 
+    the HMAC key is derived from encryption key. If however someone has got
+    this key, he will be able to trick our program with seemingly legal
+    traffic, and he cannot distinguish or get a proof, that it is us not him
+    that has sent anything.
+1.4 HMAC-SHA1 is 12 bytes shorter as SHA256.
+
 """
 
 import hashlib 
 import hmac
+import math
 from os import urandom
 from struct import pack, unpack
 
 from salsa20 import XSalsa20_xor
 
-_HASHALGO = hashlib.sha256
+_HASHALGO = hashlib.sha1
 _HASHLEN = len(_HASHALGO('').digest())
 
-_RESULT_SIZE = 0xFFFF - 24 - 2 - 32 
+_RESULT_SIZE = 0xFFFF - 24 - 2 - _HASHLEN 
 
 
 class CryptoException(Exception): pass
@@ -47,11 +69,22 @@ def decidePaddingLength(bufferLength):
         maxSize = int(min(_RESULT_SIZE - bufferLength, bufferLength * 2.0))
         return randint(0, maxSize)
 
+def KDF1(secret, length):
+    """ISO-18033-2 defined simple KDF function. Notice we assume secret is
+    already long and random enough."""
+    global _HASHALGO, _HASHLEN
+    d = int(math.ceil(length * 1.0 / _HASHLEN))
+    T = ""
+    for i in xrange(0, d):
+        C = pack("<H", i)
+        T = T + _HASHALGO(secret + C).digest()
+    return T[:length]
+KDF = KDF1
+
 class Crypto:
     def __init__(self, passphrase):
-        KEY = _HASHALGO(passphrase).digest()
-        HMACKEY = _HASHALGO(KEY).digest()
-        self.__KEY = KEY[:32]
+        self.__KEY = KDF(passphrase, 32)
+        HMACKEY = KDF(self.__KEY, _HASHLEN)
         self.__origHMAC = hmac.new(HMACKEY, '', _HASHALGO)
 
     def __HMAC(self, buf):
@@ -63,35 +96,44 @@ class Crypto:
         if len(buf) > _RESULT_SIZE: 
             raise CryptoException('Buffer too large to be encrypted.')
 
-        hmac = self.__HMAC(buf)
         buflen = len(buf)
         lenstr = pack('<H', buflen)
         padding = '0' * decidePaddingLength(buflen) # TODO discuss security here!
+        cleartext = lenstr + buf + padding
 
         iv = urandom(24) # since IV will be exposed, has to be crypto. random.
-        cleartext = lenstr + hmac + buf + padding
-        encrypted = XSalsa20_xor(cleartext, iv, self.__KEY)
-        return iv + encrypted
+        ciphertext = XSalsa20_xor(cleartext, iv, self.__KEY)
+        hmac = self.__HMAC(ciphertext)
+
+        return iv + hmac + ciphertext
 
     def decrypt(self, buf):
-        if len(buf) < 24:
+        if len(buf) < 24 + _HASHLEN:
             return False
         iv = buf[:24]
-        ciphertext = buf[24:]
-        decrypted = XSalsa20_xor(ciphertext, iv, self.__KEY)
+        hmac = buf[24:24+_HASHLEN]
+        ciphertext = buf[24+_HASHLEN:]
 
+        hmac2 = self.__HMAC(ciphertext)
+        if hmac2 != hmac: return False
+
+        decrypted = XSalsa20_xor(ciphertext, iv, self.__KEY)
         lenstr = decrypted[:2]
         buflen = unpack('<H', lenstr)[0]
-        hmac = decrypted[2:_HASHLEN+2]
-        buf = decrypted[_HASHLEN+2:][:buflen]
+        buf = decrypted[2:][:buflen]
+        if len(buf) != buflen: return False
         
-        if self.__HMAC(buf) != hmac:
-            return False
         return buf
 
 
 if __name__ == '__main__':
     encryptor = Crypto('test')
     decryptor = Crypto('test')
-    encrypted = encryptor.encrypt('hello, world' * 30)
-    print len(encrypted), decryptor.decrypt(encrypted)
+
+    import time
+    a = time.time()
+    for x in xrange(0, 1024):
+        encrypted = encryptor.encrypt('a' * 1500)
+    b = time.time()
+    print "done in %f seconds" % (b-a)
+#    print len(encrypted), decryptor.decrypt(encrypted)
---
+++ proxy.icmp.py
@@ -0,0 +1,158 @@
+
+"""
+ICMP Proxy Process
+==================
+
+This proxy utilizes the `xmpppy` library. You have to first install it before
+using this. If
+    
+    sudo pip install xmpppy
+
+doesn't work, you may have to download it from <http://xmpppy.sourceforge.net>
+and install manually.
+
+> > > CURRENTLY UNDER DEVELOPEMENT...
+"""
+
+
+import argparse
+import signal
+from select import select
+
+import xmpp
+
+from fyuneru.intsck import InternalSocketClient
+from fyuneru.droproot import dropRoot
+
+def log(x):
+    print "proxy-xmpp-client: %s" % x
+
+# ----------- parse arguments
+
+parser = argparse.ArgumentParser()
+
+# drop privilege to ...
+parser.add_argument("--uidname", metavar="UID_NAME", type=str, required=True)
+parser.add_argument("--gidname", metavar="GID_NAME", type=str, required=True)
+
+parser.add_argument("--socket", type=str, required=True)
+parser.add_argument("--peer", type=str, required=True)
+parser.add_argument("--jid", type=str, required=True)
+parser.add_argument("--password", type=str, required=True)
+
+args = parser.parse_args()
+
+dropRoot(args.uidname, args.gidname)
+
+##############################################################################
+
+class SocketXMPPProxyException(Exception): pass
+
+class SocketXMPPProxy:
+
+    def __init__(self, jid, password, peer):
+        self.__jid = xmpp.protocol.JID(jid)
+        self.__peer = xmpp.protocol.JID(peer)
+        self.__peerJIDStripped = self.__peer.getStripped()
+
+        self.xmpp = xmpp.Client(\
+            self.__jid.getDomain(),
+            debug=["always", "socket", "nodebuilder", "dispatcher"]
+        )
+        connection = self.xmpp.connect()
+        if not connection:
+            raise SocketXMPPProxyException("Unable to connect.")
+
+        authentication = self.xmpp.auth(\
+            self.__jid.getNode(),
+            password
+        )
+        if not authentication:
+            raise SocketXMPPProxyException("Authentication error.")
+
+        self.__registerHandlers()
+        self.__sendPresence()
+
+    def __registerHandlers(self):
+        self.xmpp.RegisterHandler('message', self.message)
+
+    def __sendPresence(self):
+        presence = xmpp.protocol.Presence(priority=999)
+        self.xmpp.send(presence)
+
+    recvQueue = []
+
+    def message(self, con, event):
+        msgtype = event.getType()
+        fromjid = event.getFrom().getStripped()
+        if fromjid != self.__peerJIDStripped: return
+        if msgtype in ('chat', 'normal', None):
+            body = event.getBody()
+            self.recvQueue.append(body.decode('base64'))
+
+    def send(self, buf):
+        buf = buf.encode('base64')
+        message = xmpp.protocol.Message(to=self.__peer, body=buf, typ='chat')
+        self.xmpp.send(message)
+
+##############################################################################
+
+proxy = SocketXMPPProxy(args.jid, args.password, args.peer)
+local = InternalSocketClient(args.socket)
+
+##############################################################################
+
+def doExit(signum, frame):
+    global local, proxy
+    try:
+        local.close()
+    except:
+        pass
+    try:
+        proxy.xmpp.disconnect()
+    except:
+        pass
+    print "exit now."
+    exit()
+signal.signal(signal.SIGTERM, doExit)
+
+##############################################################################
+
+sockets = {
+    proxy.xmpp.Connection._sock: 'proxy',
+    local: 'local',
+}
+
+while True:
+    try:
+        local.heartbeat()
+        r, w, _ = select(sockets.keys(), [], [], 1)
+        for each in r:
+            if sockets[each] == 'proxy':
+                proxy.xmpp.Process(1)
+                for b in proxy.recvQueue:
+                    log("Received %d bytes, sending to core." % len(b))
+                    local.send(b)
+                proxy.recvQueue = []
+
+            if sockets[each] == 'local':
+                recv = local.receive()
+                if not recv: continue
+                log("Received %d bytes, sending to tunnel." % len(recv))
+                proxy.send(recv)
+
+
+    except KeyboardInterrupt:
+        doExit(None,None)
+
+
+
+
+
+
+
+
+
+
+
+
