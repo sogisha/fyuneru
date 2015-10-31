0675c44349143bd27293b80965c26c77e978bde7
Sogisha <sogisha@users.noreply.github.com>
2015-10-31 15:52:27 +0100

    no we need UDPCONNECTOR_WORD defined

--- fyuneru/net/intsck.py
+++ fyuneru/net/intsck.py
@@ -18,6 +18,9 @@ from struct import pack, unpack
 from socket import socket, AF_UNIX, SOCK_DGRAM
 from ..util.crypto import Crypto
 
+UDPCONNECTOR_WORD = \
+    "Across the Great Wall, we can reach every corner in the world."
+
 ##############################################################################
 
 def getUNIXSocketPathByName(socketName, role):
