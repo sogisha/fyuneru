a9bc14493a56a70fbe710708c141281a09e1a383
Sogisha <sogisha@users.noreply.github.com>
2015-11-04 21:42:33 +0100

    debug empty reception

--- run_as.py
+++ run_as.py
@@ -160,6 +160,7 @@ while True:
                     # Received buffer being digested by InternalSocket itself,
                     # either some internal mechanism packet, or packet with
                     # wrong destination, or packet decryption failed...
+                    debug("Empty packet.")
                     continue
 
                 try:
